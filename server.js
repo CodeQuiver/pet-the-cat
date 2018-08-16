1274bda446e7ae86ec3a037bd611c75736ae72fe
CodeQuiver <38484031+CodeQuiver@users.noreply.github.com>
2018-08-16 16:25:24 -0400

    server basic connection set up successfully

---
+++ server.js
@@ -0,0 +1,46 @@
+//EXPRESS SETUP
+var express = require("express");
+var app = express();
+
+//BODY-PARSER STANDARD SETUP WITH EXPRESS
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var mysql = require("mysql");
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "wishing_well_db"
+});
+
+connection.connect(function(err) {
+    if(err)
+    {
+      return;
+    }
+    console.log("I am connected at " + connection.threadId);
+  });
+
+//SET UP ROUTES- if setting up here
+
+
+
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
