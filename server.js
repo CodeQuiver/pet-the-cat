187e55d2b1982e90cbd45227e4a27bafc764cc67
CodeQuiver <38484031+CodeQuiver@users.noreply.github.com>
2018-08-16 17:01:04 -0400

    completed setup of connection.js config file and its export to server and server establishing db connection

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-var mysql = require("mysql");
-
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
@@ -18,28 +16,22 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "cats_db"
+//REQUIRE other file exports
+var connection = require("./config/connection");
+
+
+//ESTABLISH CONNECTION TO DATABASE
+connection.connect(function(err) {
+  if(err)
+  {
+    console.log("error connecting to mySQL database");
+    return;
+  }
+  console.log("Connected to database at " + connection.threadId);
 });
 
-connection.connect(function(err) {
-    if(err)
-    {
-      return;
-    }
-    console.log("I am connected at " + connection.threadId);
-  });
 
-//SET UP ROUTES- if setting up here
-
-
-
-
-// Start our server so that it can begin listening to client requests.
+// START LISTENER - begin listening to client requests
 app.listen(PORT, function() {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
