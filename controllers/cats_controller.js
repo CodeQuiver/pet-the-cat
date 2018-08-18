664f9b7719823e27c35e2a8568fac3cd71362f79
CodeQuiver <38484031+CodeQuiver@users.noreply.github.com>
2018-08-18 17:18:12 -0400

    trying to write view and test the basic / route but had to start over after many unsuccessful changes to server

--- controllers/cats_controller.js
+++ controllers/cats_controller.js
@@ -1,37 +1,47 @@
 var express = require("express");
 var router = express.Router();
 
+// Importing the model to use its database functions
 var cat = require("../models/cats_model");
 
 
 //HTML routes
+router.get("/", function(req, res) {
+    cat.all(function(data) {
+      var hbsObject = {
+        cats: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
 
 //END HTML routes
 
 
-//API Routes/data routes
-router.get("/", function(req, res) {
-    //call "all" function in cats_model
-    //example placeholder- needs more structure to be actually called
-    cat.all(function(data) {
-        console.log(data);
-      });
-});
+// //API Routes/data routes
+// router.get("/", function(req, res) {
+//     //call "all" function in cats_model
+//     //example placeholder- needs more structure to be actually called
+//     cat.all(function(data) {
+//         console.log(data);
+//       });
+// });
 
-router.post("/api/cats", function(req, res) {
-    //call input/add function in cats_model
-    //example placeholder
-    cat.insert("Cleopatra", function(res){
-        console.log(res);
-      });
-});
+// router.post("/api/cats", function(req, res) {
+//     //call input/add function in cats_model
+//     //example placeholder
+//     cat.insert("Cleopatra", function(res){
+//         console.log(res);
+//       });
+// });
 
-router.put("/api/cats/:id", function(req, res) {
-    //call update function in cats_model
-    //example placeholder
-    cat.update("pet_or_not", 1, 2, function(res){
-        console.log(res);
-      });
-});
+// router.put("/api/cats/:id", function(req, res) {
+//     //call update function in cats_model
+//     //example placeholder
+//     cat.update("pet_or_not", 1, 2, function(res){
+//         console.log(res);
+//       });
+// });
 
 //END API Routes
