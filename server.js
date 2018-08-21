03c52fd4f3f17117412831b2ad1a5ecbd151fa0c
CodeQuiver <38484031+CodeQuiver@users.noreply.github.com>
2018-08-21 17:07:19 -0400

    moved html setup into handlebars files and tested static page loads correctly with all assets before adding handlebars dynamic functionality

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 var express = require("express");
 var app = express();
 
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
 //BODY-PARSER STANDARD SETUP WITH EXPRESS
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
